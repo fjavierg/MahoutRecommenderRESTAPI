4bc2b2e7c03dd4f6eb9b8d166ee74d872542b7aa
Javier <Javier@192.168.1.40>
2014-12-20 10:19:20 +0100

    Initial Commit

---
+++ basic-recommender/src/main/java/es/javiergomez/recommender/Run.java
@@ -0,0 +1,33 @@
+package es.javiergomez.recommender;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+
+public class Run {
+
+	public Run() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 * @throws TasteException 
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException, TasteException {
+
+		MyRecommender recommender;
+		List<RecommendedItem> recommendations;
+		
+		recommender = new MyUserBasedRecommender("src/main/resources/dataset.csv");
+		recommendations = recommender.getRecommendations(2, 3);
+		for (RecommendedItem recommendation : recommendations) {
+			  System.out.println(recommendation);
+			}
+
+	}
+
+}
